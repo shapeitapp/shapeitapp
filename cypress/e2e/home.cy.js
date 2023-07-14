9b87feb1736abc00745b00c25f935f86c50dcabc
Samir AMZANI <samir.amzani@gmail.com>
2023-07-14 09:14:44 +0200

    chore: test github signin

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,7 +1,14 @@
 describe('home page', () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000")
+  })
+
   it('h1 contains the correct text', () => {
-    cy.visit('http://localhost:3000')
     cy.get("[data-test='hero-heading']").contains("Shape Up dashboard")
     cy.get("[data-test='hero-heading']").contains("for GitHub Projects")
+  })
+
+  it('contains a GitHub Signing button', () => {
+    cy.get("[data-test='github-sign-in']").contains("Sign in using GitHub")
   })
 })
