62cd958148394c11c98ce12282818e11fc6bd80a
Samir AMZANI <samir.amzani@gmail.com>
2023-07-29 07:44:52 +0200

    chore: check github login

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -10,5 +10,9 @@ describe('home page', () => {
 
   it('contains a GitHub Signing button', () => {
     cy.getByData("github-sign-in").contains("Sign in using GitHub")
+    cy.getByData("github-sign-in").click()
+    cy.on("url:changed", (newUrl) => {
+      expect(newUrl).to.contain("https://github.com/login")
+    })
   })
 })
