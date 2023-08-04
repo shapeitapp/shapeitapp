c3f2878bc194c29a62c58d078e56b510899f7384
Samir AMZANI <samir.amzani@gmail.com>
2023-08-04 16:02:08 +0200

    feat: Add how to guide + basic markwdown rendering

---
+++ docs/how-to.md
@@ -0,0 +1,57 @@
+---
+title: 'How to'
+date: '2023-08-04'
+---
+
+## Add/Edit scopes
+
+You can create new scopes in two different ways:
+
+### Option 1
+
+1. Edit the Pitch or Bet issue
+2. Add you scope under the scope section
+
+See this [example](https://github.com/asyncapi/studio/issues/748)
+
+
+
+### Option 2
+
+1. Create a new issue
+2. Add this keywork in the description `related to #ISSUE_BET_NUMBER`
+
+See this [example](https://github.com/asyncapi/studio/issues/755)
+
+
+## Communicating progress
+
+You can communicate progress at any time in any issue or pull request (as long as it's part of the tracked Github project). To do so, you have to leave a comment in the issue or pull request with the following syntax:
+
+```
+/progress <percentage> [message]
+```
+or
+
+```
+/progress <percentage>
+
+A multiline message.
+It supports Markdown.
+```
+
+### Examples
+
+```
+/progress 40 We\'re still figuring out how to implement this. We have an idea but it is not yet confirmed it will work.
+```
+
+```
+/progress 50
+
+A few notes:
+* We got this figured out :tada:
+* We\'re going to use [this library](#link-to-website) to avoid losing time implementing this algorithm.
+* We decided to go for the quickest solution and will improve it if we got time at the end of the cycle.
+```
+
---
+++ src/app/guides/[id]/page.js
@@ -0,0 +1,52 @@
+import fs from 'fs'
+import path from 'path'
+import { remark } from 'remark'
+import html from 'remark-html'
+import matter from 'gray-matter'
+
+import Head from 'next/head'
+import Footer from '@/components/Footer'
+import TopNavigation from '@/components/TopNavigation'
+import Guide from '@/components/Guide'
+
+export default async function GuidePage({params}) {
+  const postsDirectory = path.join(process.cwd(), 'docs')
+  async function getDocumentation(id) {
+    const fullPath = path.join(postsDirectory, `${id}.md`);
+    const fileContents = fs.readFileSync(fullPath, 'utf8');
+  
+    // Use gray-matter to parse the post metadata section
+    const matterResult = matter(fileContents);
+  
+    // Use remark to convert markdown into HTML string
+    const processedContent = await remark()
+      .use(html)
+      .process(matterResult.content);
+    const contentHtml = processedContent.toString();
+  
+    // Combine the data with the id and contentHtml
+    return {
+      id,
+      contentHtml,
+      ...matterResult.data,
+    };
+  }
+
+  const content = await getDocumentation(params.id)
+  console.log('content', content)
+
+  return (
+    <div className="flex flex-col w-screen max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+      <TopNavigation />
+      <div className="py-8 flex-1">
+        <Head>
+          <title>Shape It! - Projects</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <Guide title={content.title} date={content.date} content={content.contentHtml}/>
+      </div>
+      <Footer />
+    </div>
+  )
+}
